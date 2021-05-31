678bb9d6326448827e1803d2bfc3fcadaf103867
oskytar <oscar.miras@gmail.com>
2021-05-31 17:37:04 +0200

    detalle piso implementado

--- README.md
+++ README.md
@@ -12,5 +12,10 @@ Vamos a crear un "clon" de Idealista que nos permita
    2. El endpoint que gestiona en POST añadirá el nuevo al piso al array de pisos
 
 5. Cuando el usuario haga un GET en '/mostrar-pisos'; vamos a renderizar una lista de pisos, pero esta vez, maquetados, con su título y su precio. Para darle más funcionalidad, vamos a modificar el array de pisos para que también contengan una foto del piso.
+6. Vamos a generar un endpoint , tipo '/piso/:idPiso'; que va a mostrar una vista detallada del piso.
+   1. La misma info que en la vista de lista de pisos
+   2. Además, la ubicación
+
+7. En la lista de pisos, se genere un href dinámico, al idPiso de la vista detallada
 
 
--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const pisos = [{
+    id: 1,
     titulo: 'Casa Rústica',
     foto: 'https://i.picsum.photos/id/1008/5616/3744.jpg?hmac=906z84ml4jhqPMsm4ObF9aZhCRC-t2S_Sy0RLvYWZwY',
     precio: 500,
+    ubicacion: 'Girona'
 }, {
+    id: 2,
     titulo: 'Palacete',
     foto: 'https://i.picsum.photos/id/101/2621/1747.jpg?hmac=cu15YGotS0gIYdBbR1he5NtBLZAAY6aIY5AbORRAngs',
-    precio: 800
+    precio: 800,
+    ubicacion: 'Barcelona'
 }]
 
 const path = require('path')
@@ -22,13 +26,13 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // Queremos que express nos informe de los datos pasados mediante POST en el cuerpo del mensaje
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 // damos accesp a la carpeta 'public' para que cualquier cliente pueda hacer un GET a cualquier recurso que se ubique en ella (hojas de estilo CSS, imágenes, documentos PDF...)
 app.use(express.static('public'));
 
 // Vamos a crear un endpoint, que cuando nos hagan un GET al directorio raiz de nuestra aplicación; mostramos la HOME PAGE de nuestro portal inmobiliario
-app.get('/', (req, res)=>{  
+app.get('/', (req, res) => {
     console.log(req)
     //Devolvemos una respuesta al cliente
     res.send(`<h1>Bievenido a Ideatonta, tu portal de confianza.</h1>`)
@@ -37,31 +41,31 @@ app.get('/', (req, res)=>{
 });
 
 // Definimos un endpoint para mostrar el listado de pisos. El endpoint será '/pisos'; que al hacer un GET, devolvera, textualmente, los pisos disponibles en la base de datos
-app.get('/pisos', (req, res)=> {
+app.get('/pisos', (req, res) => {
 
     // Obtener la querystring
     const precio = req.query.precio;
     let pisosFiltrados = pisos;
 
     if (precio) {
-        pisosFiltrados = pisos.filter( piso => piso.precio <= precio);
+        pisosFiltrados = pisos.filter(piso => piso.precio <= precio);
         console.log("Pisos Filtrados: ", pisosFiltrados)
     }
-    
-    res.send(pisosFiltrados);
-}) 
 
-app.get('/buscar', (req, res)=> {
+    res.send(pisosFiltrados);
+})
+
+app.get('/buscar', (req, res) => {
     res.render('buscador')
 })
 
 app.get('/nuevo-piso', (req, res) => {
 
-     // La mayoría de lenguajes de programación, tienen variables globales que nos permiten saber en que directorio se está ejecutando nuestra aplicación
-     console.log(__dirname);
-     console.log(__filename);
- 
-     //disponemos del método sendFile para enviar un fichero al cliente. Para poder enviar un fichero, necesiamos la ruta absoluta desde la raiz de nuestro sistema de fichero hasta el fichero que queremos enviar 
+    // La mayoría de lenguajes de programación, tienen variables globales que nos permiten saber en que directorio se está ejecutando nuestra aplicación
+    console.log(__dirname);
+    console.log(__filename);
+
+    //disponemos del método sendFile para enviar un fichero al cliente. Para poder enviar un fichero, necesiamos la ruta absoluta desde la raiz de nuestro sistema de fichero hasta el fichero que queremos enviar 
     const ruta = path.join(__dirname, 'html', 'nuevopiso.html')
     console.log("ruta: ", ruta)
 
@@ -69,7 +73,7 @@ app.get('/nuevo-piso', (req, res) => {
 })
 
 app.post('/nuevo-piso', (req, res) => {
-    
+
     const titulo = req.body.titulo; // obtenemos el campo 'titulo' del formulario 
     const precio = req.body.precio; // obtenemos el cmapo 'precio' del formulario
 
@@ -88,6 +92,40 @@ app.get('/mostrar-pisos', (req, res) => {
         totalPisos: pisos.length,
         todosLosPisos: pisos
     });
+})
+
+app.get('/piso/:idPiso', (req, res) => {
+    console.log("Id del piso del que quiero conocer información: ", req.params.idPiso) // express automáticamente nos pone tantos campo-valor en el objeto 'params' como parámetros dinámicos establezcamos para este endpoint
+
+    // Como hacemos para recuperar el elemento del array cuyo 'id' es 'idPiso' ? Mostrar por consola el objeto cuyo id sea exactamente igual a 'idPiso'.
+    const idPiso = req.params.idPiso;
+
+    
+    const piso = pisos.find(pisoCandidato => pisoCandidato.id == idPiso);
+    console.log(piso) 
+
+    // Manera 1: bucle. Buscar un objeto en concreto dentro de una lista de objetos
+    let encontrado = false;
+    let i = 0;
+
+    while (!encontrado && i < pisos.length) {
+        if (pisos[i].id == idPiso) {
+            encontrado = true;
+        }
+        else {
+            i++;
+        }
+    }
+
+        return res.render('detalle-piso', {
+            piso: piso
+        });
+
+
+    // Método 2: usar el método find de los datos de tipo array
+    // Vamos a buscar un elemento del array pisos cuyo 'id' sea el mismo que 'idPiso'
+
+
 })
 
 
